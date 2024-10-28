772724bf3d950cc3ac2fdb17b74865a58c80d1f4
Bajram Shehi <bajram.shehi98@gmail.com>
2024-10-28 18:22:03 +0100

    Services registration of repositories

---
+++ Employee_Management.DAL/DALStartup.cs
@@ -0,0 +1,29 @@
+﻿using Employee_Management.DAL.IRepositories;
+using Employee_Management.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management.DAL
+{
+    public static class DALStartup
+    {
+        public static void RegisterDALServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserProfileRepository, UserProfileRepository>();
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+            services.AddScoped<ITaskRepository, TaskRepository>();
+        }
+    }
+}
