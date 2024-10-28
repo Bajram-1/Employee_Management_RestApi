e24ee27082725abe3e5d6bbabfab3210d6901d12
Bajram Shehi <bajram.shehi98@gmail.com>
2024-10-28 18:20:13 +0100

    Users and Enums

---
+++ Employee_Management.Common/Enums/ProjectStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management.Common.Enums
+{
+    public enum ProjectStatus
+    {
+        Active = 1,
+        Completed = 2,
+        OnHold = 3
+    }
+}
---
+++ Employee_Management.Common/StaticDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management.Common
+{
+    public static class StaticDetails
+    {
+        public const string Role_Employee = "Employee";
+        public const string Role_Admin = "Admin";
+    }
+}
