b2319ca96b57187d05e62f40c42e00416d9fdd72
Bajram Shehi <bajram.shehi98@gmail.com>
2024-10-28 18:36:16 +0100

    Home Controller and View

---
+++ Employee_Management.UI/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using Employee_Management.UI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Employee_Management.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
