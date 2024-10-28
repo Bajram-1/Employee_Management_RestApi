84f9f0c727e0d00eb621ee4463102ba01e41bd1c
Bajram Shehi <bajram.shehi98@gmail.com>
2024-10-28 18:27:47 +0100

    Auth Service methods

---
+++ Employee_Management.BLL/IServices/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management.BLL.IServices
+{
+    public interface IAuthService
+    {
+        Task<string> LoginAsync(string username, string password);
+    }
+}
---
+++ Employee_Management.BLL/Services/AuthService.cs
@@ -0,0 +1,56 @@
+﻿using Employee_Management.BLL.IServices;
+using Employee_Management.DAL.IRepositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management.BLL.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly string _secret;
+        private readonly string _issuer;
+
+        public AuthService(IUserRepository userRepository, IConfiguration configuration)
+        {
+            _userRepository = userRepository;
+            _secret = configuration["JwtSettings:Secret"];
+            _issuer = configuration["JwtSettings:Issuer"];
+        }
+
+        public async Task<string> LoginAsync(string username, string password)
+        {
+            var user = await _userRepository.GetByUsernameAsync(username);
+            if (user == null || !VerifyPassword(password, user.PasswordHash))
+            {
+                throw new UnauthorizedAccessException("Invalid credentials");
+            }
+
+            // Generate JWT token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddMinutes(120),
+                Issuer = _issuer,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        private bool VerifyPassword(string password, string storedHash)
+        {
+            return password == storedHash;
+        }
+    }
+}
