969848941787cbcaef1b8b3853086b215519c003
Bajram Shehi <bajram.shehi98@gmail.com>
2024-10-28 18:34:55 +0100

    Login Controller and View

---
+++ Employee_Management.UI/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Employee_Management.BLL.DTO.ViewModels;
+using Employee_Management.BLL.IServices;
+using Employee_Management.BLL.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_Management.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly JwtTokenService _jwtTokenService;
+
+        public AccountController(IUserService userService, JwtTokenService jwtTokenService)
+        {
+            _userService = userService;
+            _jwtTokenService = jwtTokenService;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var user = await _userService.ValidateUser(model.UserName, model.Password);
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return View(model);
+            }
+
+            var token = _jwtTokenService.GenerateToken(user);
+            Response.Cookies.Append("jwt", token, new CookieOptions { HttpOnly = true, Secure = true });
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("jwt");
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
