6333b14ff64922657455d69c89ebb88da1912480
Bajram Shehi <bajram.shehi98@gmail.com>
2024-10-28 18:32:29 +0100

    JWT Service methods implementation

---
+++ Employee_Management.BLL/Services/JWTTokenService.cs
@@ -0,0 +1,51 @@
+﻿using Employee_Management.BLL.DTO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management.BLL.Services
+{
+    public class JwtTokenService
+    {
+        private readonly string _key;
+        private readonly string _issuer;
+        private readonly string _audience;
+        private readonly ILogger<JwtTokenService> _logger;
+
+        public JwtTokenService(IConfiguration config, ILogger<JwtTokenService> logger)
+        {
+            _key = config["JwtSettings:Secret"];
+            _issuer = config["JwtSettings:Issuer"];
+            _audience = config["JwtSettings:Audience"];
+        }
+
+        public string GenerateToken(Employee_Management.DAL.Entities.User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Role, user.Role)
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: _issuer,
+                audience: _audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
