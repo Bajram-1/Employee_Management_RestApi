01ad1c990f6c74b1849100b4f93209737b4c6672
Bajram Shehi <bajram.shehi98@gmail.com>
2024-10-28 18:33:51 +0100

    BLL Services registration

---
+++ Employee_Management.BLL/BLLStartup.cs
@@ -0,0 +1,24 @@
+﻿using Employee_Management.BLL.IServices;
+using Employee_Management.BLL.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management.BLL
+{
+    public static class BLLStartup
+    {
+        public static void RegisterBLLServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IUserProfileService, UserProfileService>();
+            services.AddScoped<IProjectService, ProjectService>();
+            services.AddScoped<ITaskService, TaskService>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<JwtTokenService>();
+        }
+    }
+}
