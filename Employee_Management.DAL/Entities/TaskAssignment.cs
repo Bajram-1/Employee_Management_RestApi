2b29045c8d2ea4c43439493899f9a2dd76f1a96f
Bajram Shehi <bajram.shehi98@gmail.com>
2024-11-05 14:06:24 +0100

    Task Assignment entity creation

---
+++ Employee_Management.DAL/Entities/TaskAssignment.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management.DAL.Entities
+{
+    public class TaskAssignment
+    {
+        public int TaskId { get; set; }
+        public Tasks Task { get; set; }
+
+        public int EmployeeId { get; set; }
+        public User Employee { get; set; }
+    }
+}
